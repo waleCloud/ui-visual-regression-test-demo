20ce4b2f79c7f66e66f98263f668f576952cb610
Wale Ayandiran (walecloud) <walecloud@gmail.com>
2023-09-19 12:02:45 +0100

    another change

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find in-depth information about Next.js features and API. Another change
           </p>
         </a>
 
